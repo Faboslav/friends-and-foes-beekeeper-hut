eb3bf7fb53dbd8e980d7812ec68b095432576e62
Aleš Fabiánek <fabianek.ales@gmail.com>
2024-01-20 20:39:23 +0100

    removed unused script

--- .github/scripts/changelog.sh
+++
@@ -1,6 +0,0 @@
-                    changelog=$(cat CHANGELOG.md)
-                    tag=2.0.0
-
-                    list=$(echo "$changelog" | sed -n "/^## $tag/,/^## [0-9]/p" | sed -e '1d;$d' -e '/^$/d')
-
-                    echo "$list" > RELEASE_CHANGELOG.md
