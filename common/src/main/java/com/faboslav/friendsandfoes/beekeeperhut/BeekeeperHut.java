dcf34b1c070e689c95aa529ce86dcb199aab52ce
Faboslav <fabianek.ales@gmail.com>
2025-12-25 13:43:52 +0100

    1.21.11

--- common/src/main/java/com/faboslav/friendsandfoes/beekeeperhut/BeekeeperHut.java
+++ common/src/main/java/com/faboslav/friendsandfoes/beekeeperhut/BeekeeperHut.java
@@ -1,14 +1,14 @@
 package com.faboslav.friendsandfoes.beekeeperhut;
 
 import com.faboslav.friendsandfoes.beekeeperhut.init.BeekeeperHutStructureProcessorTypes;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 
 public final class BeekeeperHut
 {
 	public static final String MOD_ID = "beekeeperhut";
 
-	public static ResourceLocation makeID(String path) {
-		return ResourceLocation.fromNamespaceAndPath(
+	public static Identifier makeID(String path) {
+		return Identifier.fromNamespaceAndPath(
 			MOD_ID,
 			path
 		);
